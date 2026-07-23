8f7d8d10a6bbf962b856d1232d09d00a9be4d00e
Simon Santus <simon.santus@gmail.com>
2026-07-23 19:28:42 +0200

    Add Category and Product models with relationships

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 
-from sqlalchemy import Column, Integer, String, DateTime, Boolean
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey, Numeric
+
+from sqlalchemy.orm import relationship
 
 from app.database import Base
 
@@ -39,3 +41,40 @@ class Category(Base):
     
     name = Column(String(20),
     nullable=False, unique=True)
+
+    products = relationship("Product",
+    back_populates="category")
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer,
+    primary_key=True)
+
+    category_id = Column(Integer,
+    ForeignKey("categories.id"),
+    nullable=False)
+
+    category = relationship("Category",
+    back_populates="products")
+
+    name = Column(String(100),
+    nullable=False)
+
+    brand = Column(String(50),
+    nullable=False)
+
+    model = Column(String(50),
+    nullable=False)
+
+    description = Column(String(1000),
+    nullable=False)
+
+    price = Column(Numeric(10, 2),
+    nullable=False)
+
+    stock = Column(Integer,
+    nullable=False, default=0)
+
+    is_active = Column(Boolean,
+    nullable=False, default=True)
