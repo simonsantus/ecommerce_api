617fc72e9481093d48e137f25646c10560e417a1
Simon Santus <simon.santus@gmail.com>
2026-07-30 12:32:33 +0200

    Clean main.py after SQLAlchemy CRUD practice

--- app/main.py
+++ app/main.py
@@ -1,22 +1,4 @@
 from app import models
-from app.models import User
-from app.database import engine, Base, SessionLocal
+from app.database import engine, Base
 
 Base.metadata.create_all(bind=engine)
-
-db = SessionLocal()
-
-user = User(
-    first_name="Simon",
-    last_name="Santus",
-    email="simon@email.com",
-    hashed_password="12345",
-)
-
-db.add(user)
-db.commit()
-db.refresh(user)
-
-print(user.id)
-
-db.close()
